3666c54e2fc723e290cf926b2b9a37347d642041
daniel-life <Daniel08222003@gmail.com>
2023-02-28 22:57:17 +0800

    modified back to top button

--- static/js/back-to-top.js
+++ static/js/back-to-top.js
@@ -1,3 +1,25 @@
-const button = document.querySelector("#btn-back-to-top");
-// BACK2TOP(selector, offset, prop, time, effect, delay);
-BACK2TOP(button, 200, "all", 300, "ease", 0);
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
